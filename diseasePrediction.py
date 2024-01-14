36f6cf318f206452d35e99e8ac7be9ebe13edc10
Gourav Bhadra <118394124+gouravbhadra2001@users.noreply.github.com>
2024-01-14 19:08:26 +0530

    Update diseasePrediction.py

--- diseasePrediction.py
+++ diseasePrediction.py
@@ -10,6 +10,13 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 # loading the saved models
 
